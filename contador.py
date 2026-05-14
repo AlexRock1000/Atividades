ec8ad9b7a79aa16f3da56dc025cde5e54fce1dc0
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-14 19:53:12 -0300

    Verificação de espaço vazio.

--- contador.py
+++ contador.py
@@ -1,12 +1,25 @@
 contador = 0
 
 while True:
-
-    numero = int(input("Digite quantos números quiser: "))
     
+    verificaçao = (input("Digite quantos números quiser: "))
 
+    #Verificação de espaço vazio.
+    if verificaçao == "":
+        print("Digite um valor.")
+        continue
+
+    #Verificação se a variável é um numero.
+    numero = int(verificaçao)
+
+    print("(Digite 0 para sair)")
+    
     if numero == 0:
         print(f"Você digitou {contador} vezes.")
         break
 
     contador += 1
+
+# variavel =input("Digite: ")
+# variavel = variavel * 2
+# print(variavel)

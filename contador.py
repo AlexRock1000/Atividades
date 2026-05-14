8fda11ee91170e85f16f751d4d516cdcff972548
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-14 19:17:11 -0300

    contador atualizado.

--- contador.py
+++ contador.py
@@ -3,8 +3,10 @@ contador = 0
 while True:
 
     numero = int(input("Digite quantos números quiser: "))
-    contador += 1
+    
 
     if numero == 0:
         print(f"Você digitou {contador} vezes.")
         break
+
+    contador += 1

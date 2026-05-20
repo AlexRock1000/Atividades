c3dadfec25f72b985e7a3e7a8cea28770d704bc7
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-20 20:39:23 -0300

    Revisão 04

---
+++ Revisão04.py
@@ -0,0 +1,14 @@
+print("")
+
+numeros = [45, 55]
+total = sum(numeros)
+
+while len(numeros) <= 4:
+    mais = input("Falta elementos, adcione mais valores: ")
+    numeros.append(mais)
+
+    print(f"O total é: {total}")
+
+    
+print("")
+

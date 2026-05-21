32fc79bb343cd6cd4215e4bddc14724184b79670
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-21 19:52:15 -0300

    aumentando o código.

--- Revisão06.py
+++ Revisão06.py
@@ -6,4 +6,28 @@ for i, produto in enumerate(produtos):
     preço = preços[i]
     print(f"Temos {produto} por R${preço}")
 
-print("")
+while True:
+    print("")
+    compra = input("Qual produto você deseja: ")
+
+    if compra == "cabeça":
+        print(f"Sua compra foi: {compra}")
+
+    elif compra == "perna":
+        print(f"Sua compra foi: {compra}")
+
+    elif compra == "braço":
+        print(f"Sua compra foi: {compra}")
+
+    elif compra == "mão":
+        print(f"Sua compra foi: {compra}")
+
+    else: print("Esse produto não temos.")
+
+    continu = input("Desja compra mias? ")
+
+    if continu == "n":
+        print("Adeus.")
+        print("")
+        exit()
+

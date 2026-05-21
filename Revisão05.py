3191e01e6aa452e29006116a882c9b20c9ac9b1a
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-20 21:41:54 -0300

    Revisão 05.

---
+++ Revisão05.py
@@ -0,0 +1,11 @@
+print("")
+
+cidades = ["Blu", "Gaspar", "Brusq"]
+
+for cidade in cidades:
+    if cidade[0] == "B":
+        continue
+    print(f"Bem vindo a cidade {cidade}")
+        
+
+print("")

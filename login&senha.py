86278a6f76766533cb73ed42ef192ae4b138ce59
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-21 21:53:43 -0300

    Colocado 3 tentativas.

--- login&senha.py
+++ login&senha.py
@@ -1,15 +1,21 @@
 print("")
 nomeCerto = "ADM"
-senhaCerto = 123
+senhaCerto = "123"
+tentativas = 0
 
 while True:
+    if tentativas == 3:
+        print("Sistema bloqueado!\n")
+        exit()
 
     print("==== LOGIN DE ACESSO ====")
     nome = input("Nome do usuário: ")
-    senha = int(input("Senha: "))
+    senha = (input("Senha: "))
 
     if nome == nomeCerto and senha == senhaCerto:
         print("\nAcesso liberado.\n")
         break
 
     else: print("\nAcesso negado!\n")
+    
+    tentativas += 1

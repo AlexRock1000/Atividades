5694b04ecf955ac4a4cd9b1176c5fc0b3e763cf5
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-05-27 19:54:21 -0300

    finalizado.

--- menu-Atividade.py
+++ menu-Atividade.py
@@ -3,6 +3,8 @@ preçoServiços = [50.00, 200.00, 150.00]
 produtos = ["Óleo", "Pneu", "Motor"]
 preçoProdutos = [40.00, 250.00, 1000.00]
 carrinho = []
+valor_Servi = 0
+valor_Prod = 0
 
 print("\nBom dia. O que você gostaria hoje?")
 
@@ -27,17 +29,21 @@ while True:
         escolhaServiço = int(input("O que deseja?\n"))
         indice_Correto = escolhaServiço - 1
 
+
         if escolhaServiço == 1:
             print(f"Você solicitou {serviços[indice_Correto]}.\n")
             carrinho.append(serviços[indice_Correto])
+            valor_Servi += preçoServiços[indice_Correto]
 
         elif escolhaServiço == 2:
             print(f"Você solicitou {serviços[indice_Correto]}.\n")
             carrinho.append(serviços[indice_Correto])
+            valor_Servi += preçoServiços[indice_Correto]
 
         elif escolhaServiço == 3:
             print(f"Você solicitou {serviços[indice_Correto]}\n")
             carrinho.append(serviços[indice_Correto])
+            valor_Servi += preçoServiços[indice_Correto]
 
         else: print("Opção inválida.\n")
     
@@ -56,24 +62,40 @@ while True:
         if escolhaProduto == 1:
             print(f"\n{produtos[i_Correta]} foi adcionado ao carrinho.\n")
             carrinho.append(produtos[i_Correta])
+            valor_Prod += preçoProdutos[i_Correta]
 
         elif escolhaProduto == 2:
             print(f"\n{produtos[i_Correta]} foi adicionado ao carrinho.\n")
             carrinho.append(produtos[i_Correta])
+            valor_Prod += preçoProdutos[i_Correta]
 
         elif escolhaProduto == 3:
             print(f"\n{produtos[i_Correta]} foi adicionado ao carrinho.\n")
             carrinho.append(produtos[i_Correta])
+            valor_Prod += preçoProdutos[i_Correta]
 
         else: print("Opção inválida.\n")
 
     else: print("\nOpção inválida.\n")
 
-    print(f"Seu carrinho contém: {carrinho}\n")
+    total = valor_Servi + valor_Prod
 
-    resposta = input("Deseja solicitar mais alguma coisa? ")
+    print(f"Seu carrinho contém: {carrinho}\n")
+    print(f"Total: R$ {total}")
+
+    resposta = input("\nDeseja solicitar mais alguma coisa? ")
     print("")
     
     if resposta in ["Não", "não", "NÃO", "n"]:
-        print("Obrigado por sua preferência.\n")
-        exit() 
+        break
+
+if total < 200:
+    print(f"Sua compra ficou no valor final de R$ {total}\n")
+    print("Obrigado por sua preferência.\n")
+    exit()
+
+else: total >= 200
+desconto = 0.90
+totalFinal = total * desconto
+print(f"Sua compra ficou no valor final com desconto de R$ {totalFinal}\n")
+print("Obrigado por sua preferência.\n")

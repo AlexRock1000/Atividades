fed81b03ea27aef09eb81fb161becc3b5db68b96
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-06-03 21:50:08 -0300

    criando conta bancária.

---
+++ Conta_Bancaria.py
@@ -0,0 +1,49 @@
+def menu():
+    opçoes = ["Depositar:", "Sacar:", "Ver Saldo:"]
+
+    print("""
+====================
+CADERNO DE TAREFAS
+====================""")
+
+    for i, opçao in enumerate(opçoes):
+        print(f"[{i+1}] {opçao}")
+    
+    print("""[0] Sair.
+====================
+    """)
+
+def Depositar():
+    saldo = []
+    print(f"Seu saldo é: {saldo}")
+    depo = int(input("Qual o valor que deseja depositar? "))
+    saldo.append(depo)
+    print(f"Seu saldo agora é de: R$ {saldo}")
+
+def Sacar():
+    saldo = []
+    print(f"Seu saldo é: {saldo}")
+    saque = int(input("Quanto deseja sacar: R$ "))
+    saldo.pop(saque)
+    print(f"Agora seu saldo é de: R$ {saldo}")
+
+def Saldo():
+    print(f"Seu saldo é de: R${saldo}")
+
+while True:
+    menu()
+
+    escolha = input(" ")
+
+    if escolha == "0":
+        print("Saindo...\n")
+        break
+
+    if escolha not in ["1","2","3","0"]:
+        print("\nOpção inválida.")
+
+    elif escolha == "1": Depositar()
+
+    elif escolha == "2": Sacar()
+
+    elif escolha == "3": Saldo()

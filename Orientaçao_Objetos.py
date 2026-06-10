60ff46c03d508b6973e3867630034ca8dc1b67cd
AlexRock1000 <davi.alexandremlk8@gmail.com>
2026-06-10 19:14:12 -0300

    colocado cadastro de p1 e p2.

--- Orientaçao_Objetos.py
+++ Orientaçao_Objetos.py
@@ -5,17 +5,20 @@ class Produto:
         self.qtd = qtd
 
     def mostrar(self):
-        print(f"{self.nome} R${self.preço:.2f} Quantidade: {self.qtd}")
+        print(f"\n{self.nome}, R$ {self.preço:.2f}, Quantidade: {self.qtd}")
 
     def cadastro(self):
         self.nome = input(f"Qual o nome do produto: ")
-        self.preço = float(f"Qual o valor: ")
-        self.qtd = int(f"Qual a quantidade: ")
+        self.preço = float(input(f"Qual o valor: "))
+        self.qtd = int(input(f"Qual a quantidade: "))
 
 p1 = Produto()
 p2 = Produto()
 
 print("")
 p1.cadastro()
+print("")
+p2.cadastro()
 p1.mostrar()
+p2.mostrar()
 print("")
